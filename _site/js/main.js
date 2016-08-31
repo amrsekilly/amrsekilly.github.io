7b73db20a92e90a16b0cbf28fbcbaeb3d80dc769
amrme <amr.e.dev@gmail.com>
2016-09-01 01:58:39 +0300

    did some speed optimizations

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,30 +1,5 @@
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
 
-// jQuery to collapse the navbar on scroll
 $(document).ready(function() {
-
-  $(window).scroll(function() {
-      if ($(".navbar").offset().top > 50) {
-          $(".navbar-fixed-top").addClass("top-nav-collapse");
-      } else {
-          $(".navbar-fixed-top").removeClass("top-nav-collapse");
-      }
-  });
-
-  // jQuery for page scrolling feature - requires jQuery Easing plugin
-  $(function() {
-      $('a.page-scroll').bind('click', function(event) {
-          var $anchor = $(this);
-          $('html, body').stop().animate({
-              scrollTop: $($anchor.attr('href')).offset().top
-          }, 1500, 'easeInOutExpo');
-          event.preventDefault();
-      });
-  });
 
   // Closes the Responsive Menu on Menu Item Click
   $('.navbar-collapse ul li a').click(function() {
@@ -39,11 +14,4 @@ $(document).ready(function() {
 
   $('[data-toggle="tooltip"]').tooltip();
 
-  // get the needed info for Freelancer.com badge
-  (function(d) {
-    var po = d.createElement('script');
-    po.type = 'text/javascript'; po.async = true; po.src = '//static.flnwdgt.com/build/js/hireme-sdk.js';
-    var s = d.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(po, s);
-  })(document);
 });

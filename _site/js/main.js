1d145a9f22d089bd79e2071bfc74f39d03afae9b
Amr Elsekilly <amr.e.dev@gmail.com>
2016-09-10 06:18:19 +0300

    added the js to the head for redirection

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,8 +1,3 @@
-var host = "amrsekilly.com";
-if ((host == window.location.host) && (window.location.protocol != 'https:')) {
-  window.location = window.location.toString().replace(/^http:/, "https:");
-}
-
 $(document).ready(function() {
   // Closes the Responsive Menu on Menu Item Click
   $('.navbar-collapse ul li a').click(function() {

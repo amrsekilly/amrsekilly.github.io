815c2435dd2e58f974c91661f010d2a395927c39
Amr Elsekilly <amr.e.dev@gmail.com>
2017-06-17 00:30:29 +0200

    fixed the navbar issue

--- _site/js/main.js
+++ _site/js/main.js
@@ -21,4 +21,24 @@ $(document).ready(function() {
       });
   });
 
+
+  /**
+ * Listen to scroll to change header opacity class
+ */
+function checkScroll(){
+    var startY = $('.navbar').height();  //The point where the navbar changes in px
+
+    if($(window).scrollTop() > startY){
+        $('.navbar').addClass("scrolled");
+    }else{
+        $('.navbar').removeClass("scrolled");
+    }
+}
+
+if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
+
 });

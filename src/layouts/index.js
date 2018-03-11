72d9985a3f0c18b1e4fa226d53bcc8af7126ebc6
Amr Elsekilly <amr.e.dev@gmail.com>
2018-03-12 01:48:40 +0200

    fixed a bug

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -20,17 +20,18 @@ class Template extends React.Component {
     this.handleCloseArticle = this.handleCloseArticle.bind(this)
   }
 
-  componentWillMount () {
-    const host = "amrsekilly.com";
-    if ((host == window.location.host) && (window.location.protocol != 'https:')) {
-      window.location = window.location.toString().replace(/^http:/, "https:");
-    }
-  }
 
   componentDidMount () {
     this.timeoutId = setTimeout(() => {
         this.setState({loading: ''});
     }, 100);
+    // redirect all traffic to use https
+    if (window) {
+      const host = "amrsekilly.com";
+      if ((host == window.location.host) && (window.location.protocol != 'https:')) {
+        window.location = window.location.toString().replace(/^http:/, "https:");
+      }
+    }
   }
 
   componentWillUnmount () {

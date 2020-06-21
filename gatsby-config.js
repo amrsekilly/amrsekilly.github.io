decda05c94452a2155e1f6b056b1dd77646fcc2f
Amr Sekilly <amr@pelcro.com>
2020-06-21 02:45:01 +0200

    Added manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,5 +70,43 @@ module.exports = {
           "https://amrsekilly.us10.list-manage.com/subscribe/post?u=9c5d18a0443adca15fe58cbcf&amp;id=520907d0ed", // add your MC list endpoint here; see plugin repo for instructions
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Amr Elsekilly",
+        short_name: "AmrSekilly",
+        start_url: "/",
+        background_color: "#000",
+        theme_color: "#000",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/assets/favicon.png",
+        icons: [
+          {
+            src: "src/assets/favicon.png",
+            purpose: "maskable",
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: "src/assets/favicon.png",
+            purpose: "maskable",
+            sizes: `196x196`,
+            type: `image/png`,
+          },
+          {
+            src: "src/assets/favicon.png",
+            sizes: `512x512`,
+            type: `image/png`,
+            purpose: "maskable",
+          },
+        ], // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
   ],
 };
